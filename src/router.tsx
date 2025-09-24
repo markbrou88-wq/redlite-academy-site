2b5e00f85cdf67a5441f56624dc99bcc884191ae
markbrou88-wq <mark.brou88@gmail.com>
2025-09-24 10:50:31 -0400

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -6,6 +6,7 @@ import App from "./App";
 import Standings from "./pages/Standings";
 import Leaders from "./pages/Leaders";
 import Games from "./pages/Games";
+import GameSummary from "./pages/GameSummary";
 
 export const router = createBrowserRouter([
   {
@@ -15,9 +16,10 @@ export const router = createBrowserRouter([
       // Landing -> redirect to standings
       { index: true, element: <Navigate to="/league/standings" replace /> },
 
-      { path: "/league/standings", element: <Standings /> },
-      { path: "/league/leaders", element: <Leaders /> },
-      { path: "/league/games", element: <Games /> },
+      { path: "league/standings", element: <Standings /> },
+      { path: "league/leaders", element: <Leaders /> },
+      { path: "league/games", element: <Games /> },
+      { path: "league/games/:slug", element: <GameSummary /> },
 
       // 404
       { path: "*", element: <Navigate to="/league/standings" replace /> },
