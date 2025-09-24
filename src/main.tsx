415b0aea36f6e515ff57b8cf435fd327a8990985
markbrou88-wq <mark.brou88@gmail.com>
2025-09-23 22:20:38 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
+// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+
+// 👇 important: load your global styles (Tailwind, etc)
+import "./index.css";
+
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
