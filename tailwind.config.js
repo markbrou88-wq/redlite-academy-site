caaca9bd5bcbdcb93b601796b842929d8ad9df18
markbrou88-wq <mark.brou88@gmail.com>
2025-09-23 22:37:26 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,2 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default { content: ["./index.html","./src/**/*.{ts,tsx}"], theme: { extend: {} }, plugins: [] }
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx,js,jsx}",   // includes App.tsx, router.tsx, pages, etc.
+  ],
+  theme: { extend: {} },
+  plugins: [],
+};
